70a7b64331c11aec32a6f9cdcbec78f8d98b5a45
Camilo Restrepo <camilorg2017@gmail.com>
2023-05-23 09:24:54 -0500

    add circular for component

--- src/webparts/appTareasSharepoint/components/AppTareasSharepoint.tsx
+++ src/webparts/appTareasSharepoint/components/AppTareasSharepoint.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { escape } from "@microsoft/sp-lodash-subset";
 import { IAppTareasSharepointProps } from "./IAppTareasSharepointProps";
+import { CircularProgress } from "@mui/material";
 
 const AppTareasSharepoint: React.FC<IAppTareasSharepointProps> = ({
   titulo,
@@ -13,6 +14,7 @@ const AppTareasSharepoint: React.FC<IAppTareasSharepointProps> = ({
         <h2>Título: {escape(titulo)}</h2>
         <div>Descripción: {escape(descripcion)}</div>
         <div>Fecha: {escape(fecha)}</div>
+        <CircularProgress />
       </div>
     </section>
   );
